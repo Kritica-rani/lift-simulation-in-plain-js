662f7445fedbd2efd091006bddee1026932af58f
Kritica Rani <120586121+Kritica-rani@users.noreply.github.com>
2022-12-16 18:16:55 +0530

    added lifts and doors for simulation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const getFloorLiftValue = document.getElementsByClassName("floorLiftValue");
 getFloorLiftValue[0].addEventListener("click", getValue);
 const floorValue = document.getElementById("inputfloors");
-const container = document.getElementsByClassName("container");
+const container = document.getElementById("container");
 const liftValue = document.getElementById("inputLifts");
 let lift = [];
 let floors = [];
@@ -10,36 +10,52 @@ function getValue() {
   //     alert("the lift must be")
   //     return;
   // }
+  console.log('container',container)
   if (floorValue.value !== "" || liftValue.value !== "") {
     document.querySelector(".form").classList.add("hide");
   }
+  // get the number entered by user
   renderFloors(floorValue.value);
   renderLifts(liftValue.value);
 }
 
 function renderFloors(floors) {
+  // generate floors
   for (i = 0; i < floors; i++) {
     let generatedFloor = document.createElement("div");
     generatedFloor.id = `floor-${i}`;
     generatedFloor.className = "floors";
-
+  // generate button
     let buttonGenerated = document.createElement("button");
     buttonGenerated.className = "callbtn";
     buttonGenerated.id = `floor-btn${i}`;
     buttonGenerated.innerHTML = "call";
     generatedFloor.appendChild(buttonGenerated);
-    container[0].appendChild(generatedFloor);
+    container.appendChild(generatedFloor);
     // floors.push(generatedFloor)
   
     
   }
 }
 function renderLifts(lifts) {
-
+  const liftSection = document.createElement('div')
+  liftSection.className="liftsection";
   for(i=0;i<lifts;i++){
     let generatedLift = document.createElement('div');
     generatedLift.id = `lift-${i}`;
     generatedLift.className="lifts";
-    container[0].appendChild(generatedLift)
+    //find bottom floor and append it to the lift
+    const bottomFloor = document.getElementById(`floor-${floorValue.value-1}`)
+    bottomFloor.className ="bottomfloor"
+    const generatedoor1 = document.createElement('div');
+    const generatedoor2 = document.createElement('div');
+    generatedoor1.className='door1';
+    generatedoor2.className='door2';
+    generatedLift.appendChild(generatedoor1);
+    generatedLift.appendChild(generatedoor2);
+    liftSection.appendChild(generatedLift);
+    bottomFloor.appendChild(liftSection);
+ 
   }
+
 }
